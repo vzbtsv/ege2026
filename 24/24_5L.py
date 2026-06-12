1f3e80ae94dbddea07cda2cadd64161d8d5bedf4
egkuleshova <8skgzvfd79@privaterelay.appleid.com>
2026-06-12 22:54:44 +0700

    Add files via upload

--- 24/24_5L.py
+++ 24/24_5L.py
@@ -1,10 +1,6 @@
 from re import *
 
 f = open('/Users/elizavetakulesova/Downloads/24_1255.txt').read()
-pat = r'(?:((XY)*(ZZX)*)*)'
+pat = r'(?:XY|ZZX)*'
 res = findall(pat, f)
-s = []
-for el in res:
-    st = ''.join(el)
-    s.append(len(st))
-print(max(s))
+print(max([len(x) for x in res]))
