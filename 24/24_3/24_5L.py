ea94696449f7f2b3c015e105214f616213e60804
vladimir <zubtsov.official@gmail.com>
2026-06-14 13:40:33 +0700

    1

--- 24/24_3/24_5L.py
+++ 24/24_3/24_5L.py
@@ -1,13 +1,20 @@
 from re import *
 
-f = open('/Users/elizavetakulesova/Downloads/24_7981.txt').read()
+f = open('24_7981.txt').read()
 cnt = 0
 ss = ['AA', 'AB', 'AC', 'BC', 'BA', 'BB', 'CC', 'CA', 'CB']
 for i in range(len(f)):
     for j in range(i + 1, len(f)):
-        if f[i] == f[j] and ('AA' not in f[i:j + 1] and 'AB' not in f[i:j + 1] and 'AC' not in f[i:j + 1] and 'BB' not in f[i:j + 1] and 'BA' not in f[i:j + 1] and 'BC' not in f[i:j + 1] and 'CC' not in f[i:j + 1] and 'CA' not in f[i:j + 1] and 'CA' not in f):
-            cnt += 1
-        if 'AA' in f[i:j + 1] or 'AB' in f[i:j + 1] or 'AC' in f[i:j + 1] or 'BB' in f[i:j + 1] or 'BA' in f[i:j + 1] or 'BC' in f[i:j + 1] or 'CC' in f[i:j + 1] or 'CA' in f[i:j + 1] or 'CA' in f:
-            break
+        st = f[i:j + 1]
+        if f[i] == f[j]:
+            if ('AA' not in f[i:j + 1] and 'AB' not in f[i:j + 1] and 'AC' not in f[i:j + 1] and 'BB' not in f[
+                                                                                                             i:j + 1] and 'BA' not in f[
+                                                                                                                                      i:j + 1] and 'BC' not in f[
+                                                                                                                                                               i:j + 1] and 'CC' not in f[
+                                                                                                                                                                                        i:j + 1] and 'CA' not in f[
+                                                                                                                                                                                                                 i:j + 1] and 'CA' not in f[i:j + 1]):
+                cnt += 1
+            else:
+                break
 
 print(cnt)
